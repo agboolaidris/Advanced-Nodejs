581421856c3d5b01a41549b0404e70d4cdd69a1d
idris agboola <agboolaisholaidreez@gmail.com>
2021-03-08 17:06:00 +0100

    update

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,15 @@
 const note = require("./note");
-console.log(note());
+const chalk = require("chalk");
+
+const command = process.argv[2];
+if (command === "add") {
+  console.log("add note!");
+} else if (command === "remove") {
+  console.log("remove note!");
+} else {
+  console.log("note!");
+}
+
 //const fs = require("fs");
 //fs.writeFileSync("note.txt", "my name is idris.");
 //fs.appendFileSync("note.txt", " I live in Laos, Nigeria");
