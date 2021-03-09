c8f978451ff7cc7941ffefcb71d7a76a469abc94
idris agboola <agboolaisholaidreez@gmail.com>
2021-03-09 16:40:05 +0100

    update

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,5 +4,9 @@ const city = "Lagos";
 const URL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e96146d3a186a7f0d764b4cd78d4e335`;
 axios
   .get(URL)
-  .then((res) => console.log(res))
+  .then((res) =>
+    console.log(
+      `it is currently ${res.data.main.temp} degrees out. there is a ${res.data.main.pressure}% chance of rain`
+    )
+  )
   .catch((err) => console.log(err));
