d07598537ac1d4a23bdcb6aa1c3ba658a5f9c95e
idris agboola <agboolaisholaidreez@gmail.com>
2021-03-09 15:42:03 +0100

    update

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const city = "Lagos";
-const URL = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=e96146d3a186a7f0d764b4cd78d4e335`;
-
+//const URL = `api.openweathermap.org/data/2.5/weather?q=lagos&appid=e96146d3a186a7f0d764b4cd78d4e335`;
+const URL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e96146d3a186a7f0d764b4cd78d4e335`;
 axios
   .get(URL)
   .then((res) => console.log(res))
